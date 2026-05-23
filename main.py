46a2d3f65f1f98e26d916d7026fee4854f6783f6
tauhid-topu <t.topu021@gmail.com>
2026-05-24 00:35:45 +0600

    handle form

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from flask import Flask,render_template,url_for
+from flask import Flask, render_template, url_for, request
 
-app = Flask(__name__,static_folder='assets',static_url_path='/assets')
+app = Flask(__name__, static_folder='assets', static_url_path='/assets')
 
 @app.route("/")
 def hello_world():
-    #static file=>dynamically generate the url
-    print(url_for('static', filename='style2.css'))
+    print(url_for('static', filename='style.css'))
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        name = request.form['username']
+        password = request.form['password']
+
+        # Here you would check database
+        return f"Welcome {name}"
+    else:
+        return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
