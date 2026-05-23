227ff1fa799d7daa390c2d6592d57aad97044fb4
tauhid-topu <t.topu021@gmail.com>
2026-05-24 00:12:01 +0600

    changing default structure

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='assets',static_url_path='/assets')
 
 @app.route("/")
 def hello_world():
+    #static file=>dynamically generate the url
+    print(url_for('static', filename='style2.css'))
     return render_template("index.html")
 
 @app.route("/login")
